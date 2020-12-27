bda60faea89f1758224a79682b07543801319102
IKLOA <fandor1q@yandex.com>
2020-12-27 20:52:05 +0300

    implemented routes and added jwt

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const config = require('config')
 
 const app = express()
 
+app.use(express.json({ extended: true }))
+
+app.use('/api/auth', require('./routes/auth'))
+
 const PORT = config.get('port') || 5000
 
 async function start() {
