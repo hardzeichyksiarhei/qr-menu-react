ae6071bfbad457e07c60cfe358956336d8ad89f5
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2020-12-31 01:22:15 +0300

    feat: add items into left navigation

--- admin/src/layouts/Default.jsx
+++ admin/src/layouts/Default.jsx
@@ -3,15 +3,16 @@ import { Outlet } from 'react-router-dom'
 
 import { Layout, Menu, Button } from 'antd'
 import {
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined,
   PoweroffOutlined,
+  AppstoreAddOutlined,
+  DashboardOutlined,
+  DeleteOutlined,
+  SettingOutlined,
+  LogoutOutlined,
 } from '@ant-design/icons'
 
 import { useAuth } from '../auth/AuthProvider'
 
-const { SubMenu } = Menu
 const { Header, Content, Sider } = Layout
 
 const Default = () => {
@@ -36,30 +37,22 @@ const Default = () => {
       </Header>
       <Layout className="default-layout__container">
         <Sider width={280} className="site-layout-background">
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100%', borderRight: 0 }}
-          >
-            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-              <Menu.Item key="1">option1</Menu.Item>
-              <Menu.Item key="2">option2</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
-              <Menu.Item key="4">option4</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-              <Menu.Item key="5">option5</Menu.Item>
-              <Menu.Item key="6">option6</Menu.Item>
-              <Menu.Item key="7">option7</Menu.Item>
-              <Menu.Item key="8">option8</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
-              <Menu.Item key="11">option11</Menu.Item>
-              <Menu.Item key="12">option12</Menu.Item>
-            </SubMenu>
+          <Menu mode="inline" style={{ height: '100%', borderRight: 0 }}>
+            <Menu.Item key="dashboard" icon={<DashboardOutlined />}>
+              Dashboard
+            </Menu.Item>
+            <Menu.Item key="menus" icon={<AppstoreAddOutlined />}>
+              My menus
+            </Menu.Item>
+            <Menu.Item key="trashMenus" icon={<DeleteOutlined />}>
+              Trash Menu
+            </Menu.Item>
+            <Menu.Item key="settings" icon={<SettingOutlined />}>
+              Settings
+            </Menu.Item>
+            <Menu.Item key="logout" icon={<LogoutOutlined />} onClick={auth.logout}>
+              Logout
+            </Menu.Item>
           </Menu>
         </Sider>
         <Layout style={{ padding: '24px' }}>
