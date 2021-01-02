44ff66f8f6495f0443807aefc072fa865701c5dd
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-01-02 04:32:39 +0300

    feat: add session expired handler

--- admin/src/plugins/axios.js
+++ admin/src/plugins/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
+import { notification } from 'antd'
 
 import store from '../store'
+import * as actions from '../store/actions/auth'
 
 axios.interceptors.request.use((request) => {
   const state = store.getState()
@@ -16,5 +18,20 @@ axios.interceptors.request.use((request) => {
 
 axios.interceptors.response.use(
   (response) => response,
-  (error) => Promise.reject(error),
+  (error) => {
+    const { status } = error.response
+
+    const state = store.getState()
+
+    if (status === 401 && state.auth.token) {
+      store.dispatch(actions.logout())
+      notification.warning({
+        message: 'Session Expired',
+        description:
+          'Logout from the Administration Panel was successful. To log in to Administration Panel, click on the "Sign in" button.',
+        duration: null,
+      })
+    }
+    return Promise.reject(error)
+  },
 )
