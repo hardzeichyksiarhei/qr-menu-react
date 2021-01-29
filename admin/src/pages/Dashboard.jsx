4949ff01a8d97022dc43c5fa608624299ae73552
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-01-29 18:04:44 +0300

    ref: delete ImagesManagement of Dashboard

--- admin/src/pages/Dashboard.jsx
+++ admin/src/pages/Dashboard.jsx
@@ -1,32 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Row, Col } from 'antd'
-
-import ImagesManagement from '../components/images/ImagesManagement'
 
 import QRCodeCard from '../components/qrCode/QRCodeCard'
 
-const Dashboard = () => {
-  const [image, setImage] = useState(null)
-
-  const handleSelectImage = (img) => {
-    console.log(img)
-    setImage(img)
-  }
-
-  return (
-    <>
-      <Row>
-        <Col span={6}>
-          <QRCodeCard />
-        </Col>
-      </Row>
-      <ImagesManagement
-        previewSettings={{ width: 104, height: 104 }}
-        onSelectImage={handleSelectImage}
-        image={image}
-      />
-    </>
-  )
-}
+const Dashboard = () => (
+  <Row>
+    <Col span={6}>
+      <QRCodeCard />
+    </Col>
+  </Row>
+)
 
 export default Dashboard
