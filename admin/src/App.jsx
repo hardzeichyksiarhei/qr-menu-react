548a4c914ee0fcf4241ff766f43929d7bdc8c371
IKLOA <fandor1q@yandex.com>
2021-01-31 23:44:57 +0300

    feat:update socket.io-client implementaion

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { message } from 'antd'
+import { addOrder } from './store/actions/orders'
 import Router from './router/Router'
+import socket from './socket'
 import { IntlProvider, LOCALES } from './intl'
 import languageSelectors from './store/selectors/language'
 
 import './scss/main.scss'
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    socket.emit('ROOM:JOIN', localStorage.getItem('userId'))
+  }, [])
+
+  useEffect(() => {
+    socket.on('ROOM:ADD_ORDER', (order) => {
+      message.info('Added new order')
+      dispatch(addOrder(order))
+    })
+  }, [dispatch])
   const { language } = useSelector(languageSelectors.language)
   return (
     <div className="App">
