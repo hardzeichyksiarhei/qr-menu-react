55943961b3666985427b1c9be80f77f49b3c305e
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-02-02 21:17:56 +0300

    feat: change category card

--- client/src/components/CardCategory/CardCategory.tsx
+++ client/src/components/CardCategory/CardCategory.tsx
@@ -19,8 +19,6 @@ function CardCategory({ category, menuId }: CardCategoryProps) {
 
   const screen = useBreakpoint()
 
-  console.log(screen.xs)
-
   return (
     <Card
       className="category-card"
@@ -45,55 +43,94 @@ function CardCategory({ category, menuId }: CardCategoryProps) {
       }
       hoverable
     >
-      <Collapse activeKey={screen.xs ? 'dishes' : ''} ghost>
-        <Panel
-          header={
-            <span className="dishes-list-header">
-              Dishes <span>{category.dishes.length} items</span>
-            </span>
-          }
-          key="dishes"
-        >
-          <List
-            itemLayout="horizontal"
-            dataSource={category.dishes}
-            renderItem={(dish) => (
-              <List.Item>
-                <div
-                  className="dish-item"
-                  onClick={() =>
-                    navigate(`/${userId}/menu/${menuId}/category/${category.id}/dish/${dish.id}`)
-                  }
-                >
-                  <div className="dish-item__photo">
-                    <Image
-                      width={65}
-                      src={
-                        dish.photo
-                          ? `${SERVER_URL}/uploads/${dish.photo.userId}/thumbnail/${dish.photo.sizes.thumbnail}`
-                          : 'https://via.placeholder.com/80x80?text=QR Menu'
-                      }
-                      preview={false}
-                    />
-                  </div>
-                  <div className="dish-item__content">
-                    <h4 className="dish-item__title">{dish.title}</h4>
-                    <div className="dish-item__meta">
-                      {dish.ingredients.length ? (
-                        <span>{dish.ingredients.length} ingredients</span>
-                      ) : null}
-                      {dish.tags.length ? <span>{dish.tags.length} tags</span> : null}
-                      {dish.allergens.length ? (
-                        <span>{dish.allergens.length} allergens</span>
-                      ) : null}
-                    </div>
+      {screen.xs ? (
+        <List
+          itemLayout="horizontal"
+          dataSource={category.dishes}
+          renderItem={(dish) => (
+            <List.Item>
+              <div
+                className="dish-item"
+                onClick={() =>
+                  navigate(`/${userId}/menu/${menuId}/category/${category.id}/dish/${dish.id}`)
+                }
+              >
+                <div className="dish-item__photo">
+                  <Image
+                    width={65}
+                    src={
+                      dish.photo
+                        ? `${SERVER_URL}/uploads/${dish.photo.userId}/thumbnail/${dish.photo.sizes.thumbnail}`
+                        : 'https://via.placeholder.com/80x80?text=QR Menu'
+                    }
+                    preview={false}
+                  />
+                </div>
+                <div className="dish-item__content">
+                  <h4 className="dish-item__title">{dish.title}</h4>
+                  <div className="dish-item__meta">
+                    {dish.ingredients.length ? (
+                      <span>{dish.ingredients.length} ingredients</span>
+                    ) : null}
+                    {dish.tags.length ? <span>{dish.tags.length} tags</span> : null}
+                    {dish.allergens.length ? <span>{dish.allergens.length} allergens</span> : null}
                   </div>
                 </div>
-              </List.Item>
-            )}
-          />
-        </Panel>
-      </Collapse>
+              </div>
+            </List.Item>
+          )}
+        />
+      ) : (
+        <Collapse ghost>
+          <Panel
+            header={
+              <span className="dishes-list-header">
+                Dishes <span>{category.dishes.length} items</span>
+              </span>
+            }
+            key="dishes"
+          >
+            <List
+              itemLayout="horizontal"
+              dataSource={category.dishes}
+              renderItem={(dish) => (
+                <List.Item>
+                  <div
+                    className="dish-item"
+                    onClick={() =>
+                      navigate(`/${userId}/menu/${menuId}/category/${category.id}/dish/${dish.id}`)
+                    }
+                  >
+                    <div className="dish-item__photo">
+                      <Image
+                        width={65}
+                        src={
+                          dish.photo
+                            ? `${SERVER_URL}/uploads/${dish.photo.userId}/thumbnail/${dish.photo.sizes.thumbnail}`
+                            : 'https://via.placeholder.com/80x80?text=QR Menu'
+                        }
+                        preview={false}
+                      />
+                    </div>
+                    <div className="dish-item__content">
+                      <h4 className="dish-item__title">{dish.title}</h4>
+                      <div className="dish-item__meta">
+                        {dish.ingredients.length ? (
+                          <span>{dish.ingredients.length} ingredients</span>
+                        ) : null}
+                        {dish.tags.length ? <span>{dish.tags.length} tags</span> : null}
+                        {dish.allergens.length ? (
+                          <span>{dish.allergens.length} allergens</span>
+                        ) : null}
+                      </div>
+                    </div>
+                  </div>
+                </List.Item>
+              )}
+            />
+          </Panel>
+        </Collapse>
+      )}
     </Card>
   )
 }
