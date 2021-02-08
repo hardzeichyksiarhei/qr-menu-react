4b8af2a08674d66099e9933487af13bb79075d88
IKLOA <fandor1q@yandex.com>
2021-02-08 14:09:33 +0300

    add order to localStorage

--- client/src/store/reducers/order.js
+++ client/src/store/reducers/order.js
@@ -1,8 +1,8 @@
 import * as types from '../types/order'
 
 const initialState = {
-  items: [],
-  totalPrice: 0,
+  items: JSON.parse(localStorage.getItem('order')).items || [],
+  totalPrice: JSON.parse(localStorage.getItem('order')).totalPrice | 0,
   currency: null,
 }
 
@@ -13,15 +13,18 @@ const reducer = (state = initialState, action) => {
 
       const itemIdx = state.items.findIndex(({ item }) => item.id === addItem.id)
 
+      state.items =
+        itemIdx !== -1
+          ? state.items.map(({ item, quantity }, idx) =>
+              itemIdx === idx ? { quantity: quantity + 1, item } : { quantity, item },
+            )
+          : [...state.items, { quantity: 1, item: addItem }]
+
+      state.totalPrice = state.totalPrice + addItem.priceValue
+
+      localStorage.setItem('order', JSON.stringify(state))
       return {
         ...state,
-        items:
-          itemIdx !== -1
-            ? state.items.map(({ item, quantity }, idx) =>
-                itemIdx === idx ? { quantity: quantity + 1, item } : { quantity, item },
-              )
-            : [...state.items, { quantity: 1, item: addItem }],
-        totalPrice: state.totalPrice + addItem.priceValue,
       }
     }
 
@@ -32,19 +35,23 @@ const reducer = (state = initialState, action) => {
 
       if (state.items[itemIdx].quantity === 1) {
         const newItems = state.items.filter((el) => el.item.id !== deleteItem.id)
+        state.totalPrice = state.totalPrice - deleteItem.priceValue
+        state.items = newItems
+        localStorage.setItem('order', JSON.stringify(state))
         return {
           ...state,
-          items: newItems,
-          totalPrice: state.totalPrice - deleteItem.priceValue,
         }
       }
 
+      state.items = state.items.map(({ item, quantity }, idx) =>
+        itemIdx === idx ? { quantity: quantity - 1, item } : { quantity, item },
+      )
+      state.totalPrice = state.totalPrice - deleteItem.priceValue
+
+      localStorage.setItem('order', JSON.stringify(state))
+
       return {
         ...state,
-        items: state.items.map(({ item, quantity }, idx) =>
-          itemIdx === idx ? { quantity: quantity - 1, item } : { quantity, item },
-        ),
-        totalPrice: state.totalPrice - deleteItem.priceValue,
       }
     }
 
